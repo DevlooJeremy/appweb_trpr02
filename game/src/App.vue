6731cab20c562591dc46386c60ea150e3bdffa5c
Jeremy <jeremydevloo@gmail.com>
2024-04-04 17:43:13 -0400

    routing completed

--- game/src/App.vue
+++ game/src/App.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+import {RouterView} from 'vue-router'
 </script>
 
 <template>
+    <header>
+        <div>
+            nav XD
+        </div>
+    </header>
+    <main>
+        
+        <RouterView/>
+    </main>
 
 </template>
 
