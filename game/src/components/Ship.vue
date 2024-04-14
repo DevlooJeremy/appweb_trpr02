9fd7aeedc364c75544ddaa605876e5f1914220be
GlucoseLeGlock <minigo17@outlook.com>
2024-04-13 20:20:29 -0400

    game interface done

--- game/src/components/Ship.vue
+++ game/src/components/Ship.vue
@@ -3,8 +3,17 @@
 </script>
 
 <template>
-
-</template>
+    <div class="container text-white">
+        <div class="bg-blue p-2 rounded-top">
+            *Name*
+        </div>
+        <div class="bg-dark p-4 rounded-bottom">
+            <div>Maitre - *Nbr* CG</div>
+            <div class="my-3">*Ship Name*</div>
+            <div class="bg-blue text-center rounded">%</div>
+        </div>
+    </div>
+    </template>
 
 <style>
 </style>
