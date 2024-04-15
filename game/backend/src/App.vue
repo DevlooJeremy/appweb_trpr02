6bee58c4a1ed6253b0f3046a6953366c130e2f94
GlucoseLeGlock <minigo17@outlook.com>
2024-04-14 23:42:10 -0400

    first deployment

---
+++ game/backend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {RouterView} from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <div>
+            nav XD
+        </div>
+    </header>
+    <main>
+        
+        <RouterView/>
+    </main>
+
+</template>
+
+<style scoped>
+
+</style>
