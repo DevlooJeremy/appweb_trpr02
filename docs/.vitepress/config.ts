c06ed61934f6b54b79c7ed63c8fd02e1cc2f7700
GlucoseLeGlock <minigo17@outlook.com>
2024-04-16 13:47:17 -0400

    DEPLOY!

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,24 +3,27 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Revue de code TP2",
-  description: "Revue de code du TP2 documentée de Hugo Larochelle et Jeremy Devloo",
+  description: "Revue de code du TP2 de Hugo Larochelle et Jeremy Devloo",
   base: "/appweb_trpr02/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Revue Hugo', link: '/hugo-larochelle' },
-      { text: 'Revu Jérémy', link: '/jeremy-devloo' }
+      { text: 'Examples', link: '/markdown-examples' }
     ],
 
     sidebar: [
       {
         text: 'Examples',
         items: [
-          { text: 'Revue Hugo', link: '/hugo-larochelle' },
-          { text: 'Revu Jérémy', link: '/jeremy-devloo' }
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
         ]
       }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
   }
 })
